86c66150a2506d0956ab4d4d07aa279e76dd1392
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-11-12 17:51:35 -0500

    Convex Setup Completed

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const PodcastCard = ({
@@ -9,7 +10,21 @@ const PodcastCard = ({
     podcastId: number
 }) => {
     return (
-        <div className='cursor-pointer'>PodcastCard</div>
+        <div className='cursor-pointer'>PodcastCard
+        <figure className='flex flex-col gap-2'>
+            <Image
+            src={imgUrl}
+            width={174}
+            height={174}
+            alt={title}
+            className='aspect-square h-fit w-full rounded-xl 2xl:size-[200px]'
+             />
+            <div className='flex flex-col'>
+                <h1 className='text-16 truncate font-bold text-white-1'>{title}</h1>
+                <h2 className='text-12 truncate font-normal capitalize text-white-4'>{description}</h2>
+            </div>
+        </figure>
+        </div>
     )
 }
 
