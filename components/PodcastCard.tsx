794d7560db5e74536832bfbda0de91e3aaf31b4f
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-11-12 16:49:40 -0500

    Right Sidebar Completed - Starting Homepage Layout

---
+++ components/PodcastCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PodcastCard = ({
+    imgUrl, title, description, podcastId
+}: {
+    imgUrl: string,
+    title: string,
+    description: string,
+    podcastId: number
+}) => {
+    return (
+        <div className='cursor-pointer'>PodcastCard</div>
+    )
+}
+
+export default PodcastCard
