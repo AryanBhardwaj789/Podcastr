0fcf0d2e5b98fef30d722369ab39e9e45ec6a04b
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-11-13 18:55:04 -0500

    Publishing Podcast Functionality Done!

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -30,6 +30,10 @@ import GeneratePodcast from "@/components/GeneratePodcast"
 import GenerateThumbnail from "@/components/GenerateThumbnail"
 import { Loader } from "lucide-react"
 import { Id } from "@/convex/_generated/dataModel"
+import { useToast } from "@/hooks/use-toast"
+import { useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { useRouter } from "next/navigation"
 
 const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
 
@@ -39,6 +43,7 @@ const formSchema = z.object({
 })
 
 const CreatePodcast = () => {
+  const router = useRouter();
   const [audioStorageId, setAudioStorageId] = useState<Id<"_storage"> | null>(null)
   const [audioDuration, setAudioDuration] = useState(0);
   const [audioUrl, setAudioUrl] = useState('');
@@ -51,6 +56,8 @@ const CreatePodcast = () => {
   const [voicePrompt, setVoicePrompt] = useState('');
 
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const createPodcast = useMutation(api.podcasts.createPodcast); 
+  const { toast } = useToast();
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
@@ -62,10 +69,36 @@ const CreatePodcast = () => {
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    try {
+      setIsSubmitting(true);
+      if(!audioUrl || !imageUrl || !voiceType) {
+        toast({title: 'Please generate audio and image'})
+        setIsSubmitting(false);
+        throw new Error('Please generate audio and image');
+      }
+
+      const podcast = await createPodcast({
+        podcastTitle: data.podcastTitle,
+        podcastDescription: data.podcastDescription,
+        audioUrl,
+        imageUrl,
+        voiceType,
+        imagePrompt,
+        voicePrompt,
+        views: 0,
+        audioDuration,
+        audioStorageId: audioStorageId!,
+        imageStorageId: imageStorageId!,
+      })
+      toast({title: 'Podcast Created'})
+      setIsSubmitting(false)
+      router.push('/')
+    } catch (error) {
+      console.log(error)
+      toast({title: 'Error', variant:'destructive'})
+      setIsSubmitting(false);
+    }
   }
 
   return (
--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,16 +1,22 @@
+import { PodcastCardProps } from '@/types'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 const PodcastCard = ({
     imgUrl, title, description, podcastId
-}: {
-    imgUrl: string,
-    title: string,
-    description: string,
-    podcastId: number
-}) => {
+}: PodcastCardProps) => {
+
+    const router = useRouter();
+    const handleViews = () => {
+        // increase views
+        router.push(`/podcasts/${podcastId}`, {
+            scroll: true
+        })
+    }
+
     return (
-        <div className='cursor-pointer'>PodcastCard
+        <div className='cursor-pointer' onClick={handleViews}>PodcastCard
         <figure className='flex flex-col gap-2'>
             <Image
             src={imgUrl}
