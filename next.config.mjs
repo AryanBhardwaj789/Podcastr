764640e85ae5f91ec13bc5fa325bce7dcb0db70e
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-11-13 19:27:49 -0500

    Podcast details page created with its Functionality

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'earnest-jay-755.convex.cloud'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
       }
     ]
   }
